06a4fe95c8fc425f7c70288e3699305afbf2e127
nastymorbol <nastymorbol@gmail.com>
2018-09-18 23:08:18 +0200

    Changed Type to Library and created Example Project

---
+++ CLIAnimation/CLIGraphPlotter.Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace CLIGraphPlotter.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 80;
+            int height = 40;
+            
+            if(!int.TryParse(args.ElementAtOrDefault(0), out width)) width = 80;
+            if(!int.TryParse(args.ElementAtOrDefault(1), out height)) height = 40;
+
+            CLIGraphPlotter pltr = new CLIGraphPlotter(width, height / 2);
+            CLIGraphPlotter pltr2 = new CLIGraphPlotter(width, height / 2, 0, height / 2 + 1);
+
+            Console.Clear();
+            var random = new Random();
+            while (true)
+            {
+                Console.CursorVisible = false;
+                pltr.Update(random.NextDouble() * 100.0);
+                pltr2.Update(random.NextDouble() * 100.0);
+                Console.CursorVisible = true;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}

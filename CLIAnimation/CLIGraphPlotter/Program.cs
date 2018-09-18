aeb547e92e4b3592735301c82b6212c98f0efc14
nastymorbol <nastymorbol@gmail.com>
2018-09-18 23:00:12 +0200

    Implemented Startposition X + Y

--- CLIAnimation/CLIGraphPlotter/Program.cs
+++ CLIAnimation/CLIGraphPlotter/Program.cs
@@ -12,137 +12,24 @@ namespace CLIGraphPlotter
     {
         static void Main(string[] args)
         {
-            CLIGraphPlotter pltr = new CLIGraphPlotter(70, 20);
+            int width = 80;
+            int height = 40;
+            
+            int.TryParse(args.ElementAtOrDefault(0), out width);
+            int.TryParse(args.ElementAtOrDefault(1), out height);
 
-            PerformanceCounter cpuCounter;
-            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            CLIGraphPlotter pltr = new CLIGraphPlotter(width, height / 2);
+            CLIGraphPlotter pltr2 = new CLIGraphPlotter(width, height / 2, 0, height / 2 + 1);
 
+            Console.Clear();
+            var random = new Random();
             while (true)
             {
-                pltr.Update(cpuCounter.NextValue());
-                Thread.Sleep(1000);
-            }
-        }
-
-        public class CLIGraphPlotter
-        {
-            private int _width;
-            private int _height;
-            private GraphPixel[] _data;
-            private ConsoleColor _defaultForegroundColor = ConsoleColor.White;
-            private int _startPositionX;
-            private int _startPositionY;
-            public char EmptyChar = ' ';
-            public char PixelChar = '■';
-
-            public CLIGraphPlotter()
-            {
-                _width = Console.BufferWidth - 4;
-                _height = 20;
-
-                _data = new GraphPixel[_width];
-            }
-
-            public CLIGraphPlotter(int width, int height, int startPositionX = 0, int startPositionY = 0)
-            {
-                if (width > Console.BufferWidth - 4)
-                {
-                    width = Console.BufferWidth - 4;
-                }
-
-                _width = width;
-                _height = height;
-                _startPositionX = startPositionX;
-                _startPositionY = startPositionY;
-                _data = new GraphPixel[_width];
-            }
-
-            public void Update(double newValueInPercent)
-            {
-                Array.Copy(_data, 1, _data, 0, _data.Length - 1);
-
-                int pixels = (int)Math.Floor((newValueInPercent / 100 * (_height - 1)));
-
-                var pixelData = new GraphPixel();
-                pixelData.Pixels = pixels;
-
-                if (newValueInPercent > 95)
-                {
-                    pixelData.ForegroundColor = ConsoleColor.DarkRed;
-                }
-                else if (newValueInPercent > 75)
-                {
-                    pixelData.ForegroundColor = ConsoleColor.Red;
-                }
-                else if (newValueInPercent > 50)
-                {
-                    pixelData.ForegroundColor = ConsoleColor.Green;
-                }
-                else
-                {
-                    pixelData.ForegroundColor = ConsoleColor.DarkGreen;
-                }
-
-                _data[_data.Length - 1] = pixelData;
-
-                PlotData();
-            }
-
-            private void PlotData()
-            {
-                for (int x = _width; x >= 0; x--)
-                {
-                    for (int y = _height; y >= 0; y--)
-                    {
-                        Console.ForegroundColor = _defaultForegroundColor;
-
-                        if (y == 0 && x == 0)
-                        {
-                            SetPosition(x + 1, _height + 1);
-                            Console.Write("+");
-                        }
-                        else if (x == 0)
-                        {
-                            SetPosition(x + 1, y);
-                            Console.Write("|");
-                        }
-                        else if (y == 0)
-                        {
-                            SetPosition(x + 2, _height + 1);
-                            Console.Write("-");
-                        }
-                        else
-                        {
-                            SetPosition(x + 2, y);
-
-                            if ((_height - y) < _data[x - 1].Pixels)
-                            {
-                                Console.ForegroundColor = _data[x - 1].ForegroundColor;
-                                Console.WriteLine(PixelChar);
-                            }
-                            else
-                            {
-                                Console.WriteLine(EmptyChar);
-                            }
-                        }
-                    }
-                }
-            }
-
-            private void SetPosition(int x, int y)
-            {
-                try
-                {
-                    Console.SetCursorPosition(x, y);
-                }
-                catch { }
-            }
-
-            public struct GraphPixel
-            {
-                public int Pixels { get; set; }
-                public ConsoleColor ForegroundColor { get; set; }
-                public ConsoleColor BackgroundColor { get; set; }
+                Console.CursorVisible = false;
+                pltr.Update(random.NextDouble() * 100.0);
+                pltr2.Update(random.NextDouble() * 100.0);
+                Console.CursorVisible = true;
+                Thread.Sleep(5000);
             }
         }
     }
---
+++ CLIAnimation/CLIGraphPlotter/src/GraphPixel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CLIGraphPlotter
+{
+            public struct GraphPixel
+            {
+                public int Pixels { get; set; }
+                public ConsoleColor ForegroundColor { get; set; }
+                public ConsoleColor BackgroundColor { get; set; }
+            }
+}
